9b74220dd452bdd73b0170b89da42673a6057fd2
Kyle Oliveri <kyle@Kyles-MacBook-Air.local>
2021-06-08 17:05:44 -0700

    added env file

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+// const config = require('config');
+// const db = config.get('mongoURI');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const url = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(url, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
