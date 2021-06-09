fcf407ede0748323291d5e91d9a5ae54502c3416
Kyle Oliveri <kyle.a.oliveri@gmail.com>
2021-06-08 21:58:45 -0700

    added node env

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-// const config = require('config');
-// const db = config.get('mongoURI');
-const dotenv = require('dotenv');
+const config = require('config');
+const db = config.get('mongoURI');
+// const dotenv = require('dotenv');
 
-dotenv.config();
-const url = process.env.MONGO_URI;
+// dotenv.config();
+// const url = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(url, {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
